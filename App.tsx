6746b2c4833b241e1b7ad62445a24b9a1bdac29f
joaomalfatti <joao.cursos1210@gmail.com>
2024-08-05 21:33:55 -0400

    Adicionando componente Loading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
-import { ActivityIndicator } from 'react-native';
-
 import { ThemeProvider } from 'styled-components';
-
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
+
 
 import theme from './src/theme';
 
@@ -22,7 +21,7 @@ export default function App() {
     
     /* aqui centraliza toda tema do aplicativo*/
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
     
   );
